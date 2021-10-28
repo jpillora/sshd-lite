8da364e2f554f96be13e3a57bb5bc248a8449fb7
Greg Broiles <gbroiles@gmail.com>
2021-10-28 21:03:06 +0000

    removed unnecessary <auth> stuff

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 var version string = "0.0.0-src" //set via ldflags
 
 var help = `
-  Usage: sshd-lite [options] <auth>
+  Usage: gosshpot [options] 
 
   Version: ` + version + `
 
@@ -26,19 +26,10 @@ var help = `
     --version, display version
     -v, verbose logs
 
-  <auth> must be set to one of:
-    1. a username and password string separated by a colon ("user:pass")
-    2. a path to an ssh authorized keys file ("~/.ssh/authorized_keys")
-    3. "none" to disable client authentication :WARNING: very insecure
-
   Notes:
     * if no keyfile and no keyseed are set, a random RSA2048 key is used
-    * once authenticated, clients will have access to a shell of the
-    current user. sshd-lite does not lookup system users.
-    * sshd-lite only supports remotes shells. tunnelling and command
-    execution are not currently supported.
 
-  Read more: https://github.com/jpillora/sshd-lite
+  Read more: https://github.com/gbroiles/gosshpot
 
 `
 
@@ -73,12 +64,6 @@ func main() {
 	if *h1f || *h2f {
 		flag.Usage()
 	}
-
-	args := flag.Args()
-	if len(args) != 1 {
-		flag.Usage()
-	}
-	c.AuthType = args[0]
 
 	s, err := gosshpot.NewServer(c)
 	if err != nil {
