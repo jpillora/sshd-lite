f5d5458ab6224335a5f7a6343fe3fae2043601db
Jaime Pillora <dev@jpillora.com>
2025-11-08 03:52:47 +0000

    fix: unix/macos exec fork bug (fixes #22)
    i blame claude

--- server/exec_unix.go
+++ server/exec_unix.go
@@ -1,16 +1,12 @@
+//go:build !windows
 // +build !windows
 
 package sshd
 
 import (
 	"os/exec"
-	"syscall"
 )
 
-// setSysProcAttr configures the command to run in a new process group on Unix systems.
-// This allows the process to be killed independently from the parent process.
 func setSysProcAttr(cmd *exec.Cmd) {
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Setpgid: true,
-	}
+	// no special flags needed for unix-like systems
 }
