72807cdc838f935d0a69d472f2a82b662942f35a
Jaime Pillora <dev@jpillora.com>
2026-01-06 19:41:02 +1100

    fix help text

--- main.go
+++ main.go
@@ -15,20 +15,20 @@ import (
 var version string = "0.0.0-src" //set via ldflags
 
 const notes = `
-  <auth> must be set to one of:
-    1. a username and password string separated by a colon ("myuser:mypass")
-    2. a path to an ssh authorized keys file ("~/.ssh/authorized_keys")
-    3. an authorized github user ("github.com/myuser") public keys from .keys
-    4. "none" to disable client authentication :WARNING: very insecure
+<auth> must be set to one of:
+1. a username and password string separated by a colon ("myuser:mypass")
+2. a path to an ssh authorized keys file ("~/.ssh/authorized_keys")
+3. an authorized github user ("github.com/myuser") public keys from .keys
+4. "none" to disable client authentication :WARNING: very insecure
 
-  Notes:
-    * if no keyfile and no keyseed are set, a random RSA2048 key is used
-    * authorized_key files are automatically reloaded on change
-    * once authenticated, clients will have access to a shell of the
-      current user. sshd-lite does not lookup system users.
-    * sshd-lite only supports remotes shells, sftp, and tcp forwarding. command
-      execution are not currently supported.
-    * sftp working directory is the home directory of the user
+Notes:
+* if no keyfile and no keyseed are set, a random RSA2048 key is used
+* authorized_key files are automatically reloaded on change
+* once authenticated, clients will have access to a shell of the
+  current user. sshd-lite does not lookup system users.
+* sshd-lite only supports remotes shells, sftp, and tcp forwarding. command
+  execution are not currently supported.
+* sftp working directory is the home directory of the user
 `
 
 func main() {
